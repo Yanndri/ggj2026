65b31a9e271f68a7dbcd6bf6eb08a69e2bf5273e
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2026-01-31 03:05:13 +0800

    chore: fix npc movement

--- Managers/game_manager.gd
+++ Managers/game_manager.gd
@@ -8,7 +8,7 @@ signal on_contamination_progress_update(progress: int)
 func addContaminationProgress() -> void:
 	contaminationProgress += 1
 	on_contamination_progress_update.emit(contaminationProgress)
-	print('contamination progree', contaminationProgress/100)
+	#print('contamination progree', contaminationProgress/100)
 
 func getContaminationProgress() -> int:
 	return contaminationProgress / 100
--- NPC/npc.gd
+++ NPC/npc.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var platform_area: CollisionShape2D = $"../Area2D/CollisionShape2D"
+@onready var platform_area: CollisionShape2D = %CollisionShape2D
 @onready var warning_area: Area2D = $WarningArea
 @onready var danger_area: Area2D = $DangerArea
 @onready var direction_cooldown_timer: Timer = $DirectionCooldownTimer
@@ -11,6 +11,7 @@ var speed: int = 100
 var direction: Vector2
 var isStillOnDangerArea: bool = false
 var isHovered: bool = false
+var isPickedUp: bool = false
 var contaminationInterval: int = 1
 var timeElepsed: float = 0.0
 
@@ -21,37 +22,32 @@ var platformAreaY
 func _ready() -> void:
 	randomize()
 	_change_direction()
-	platformAreaX = platform_area.get_viewport_rect().size.x
-	platformAreaY = platform_area.get_viewport_rect().size.y
 	print([platformAreaX,platformAreaY ])
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left_click") and isHovered:
+	if Input.is_action_pressed("left_click") and isPickedUp:
 		position = get_global_mouse_position()
+	else:
+		# Clamp to boundary
+		position.x = clamp(position.x, 0, platformAreaX)
+		position.y = clamp(position.y, 0, platformAreaY)
+		
+		# Change direction if hitting edge
+		if position.x <= 0 or position.x >= platformAreaX or \
+		   position.y <= 0 or position.y >= platformAreaY:
+			_change_direction()
+		
+		position += direction * speed * delta
 	
-	position += direction * speed * delta
-	
-	# Clamp to boundary
-	position.x = clamp(position.x, 0, platformAreaX)
-	position.y = clamp(position.y, 0, platformAreaY)
-	
-	# Change direction if hitting edge
-	if position.x <= 0 or position.x >= platformAreaX or \
-	   position.y <= 0 or position.y >= platformAreaY:
-		_change_direction()
-	
-	position += direction * speed * delta
 	timeElepsed += delta
-	
-	#print(position, platform_area.get_viewport_rect().size)
-	
+
 	if timeElepsed >= contaminationInterval:
 		if isStillOnDangerArea: 
 			GameManager.addContaminationProgress()
 		
 func _change_direction() -> void:
-	platformAreaX = platform_area.get_viewport_rect().size.x
-	platformAreaY = platform_area.get_viewport_rect().size.y
+	platformAreaX = platform_area.shape.get_rect().size.x
+	platformAreaY = platform_area.shape.get_rect().size.y
 	direction = Vector2(randi_range(-platformAreaX, platformAreaX), randi_range(-platformAreaY, platformAreaY)).normalized()
 	direction_cooldown_timer.start()
 
@@ -69,3 +65,19 @@ func _on_danger_area_area_exited(area: Area2D) -> void:
 
 func _on_direction_cooldown_timer_timeout() -> void:
 	_change_direction()
+
+
+func _on_mouse_entered() -> void:
+	isHovered = true
+
+
+func _on_mouse_exited() -> void:
+	isHovered = false
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("left_click"):
+		isPickedUp = true
+	
+	if event.is_action_released("left_click"):
+		isPickedUp = false
