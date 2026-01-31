28bc4bfae4e67484a240b7c78cf83a975854a3f1
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2026-01-31 11:24:17 +0800

    chore: update animation sprite

--- NPC/npc.gd
+++ NPC/npc.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
+var sprite_list: Dictionary = {
+	'sprite_1': preload("res://Assets/PixelClique/BaristaGirl/BaristaGirlMovement&Gestures.png"),
+	'sprite_2': preload("res://Assets/PixelClique/BeretGirl/BeretGirlMovement&Gestures.png"),
+	'sprite_3': preload("res://Assets/PixelClique/EmoGirl/EmoGirlMovement.png"),
+	'sprite_4': preload("res://Assets/PixelClique/GymGirl/GymGirlMovement&Gestures.png")
+}
+
 @onready var platform_area: CollisionShape2D = %CollisionShape2D
 @onready var warning_area: Area2D = $WarningArea
 @onready var danger_area: Area2D = $DangerArea
 @onready var direction_cooldown_timer: Timer = $DirectionCooldownTimer
-@onready var danger_entered_timer: Timer = $DangerEnteredTimer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 var speed: int = 100
 var direction: Vector2
 var isStillOnDangerArea: bool = false
-var isHovered: bool = false
 var isPickedUp: bool = false
 var contaminationInterval: int = 1
 var timeElepsed: float = 0.0
@@ -22,9 +29,19 @@ var platformAreaY
 func _ready() -> void:
 	randomize()
 	_change_direction()
-	print([platformAreaX,platformAreaY ])
+	sprite_2d.texture = sprite_list.values().pick_random()
 
 func _process(delta: float) -> void:
+	_move(delta)
+	_animate_sprite()
+	
+	timeElepsed += delta
+
+	if timeElepsed >= contaminationInterval:
+		if isStillOnDangerArea: 
+			GameManager.addContaminationProgress()
+			
+func _move(delta: float) -> void:
 	if Input.is_action_pressed("left_click") and isPickedUp:
 		position = get_global_mouse_position()
 	else:
@@ -38,18 +55,21 @@ func _process(delta: float) -> void:
 			_change_direction()
 		
 		position += direction * speed * delta
-	
-	timeElepsed += delta
-
-	if timeElepsed >= contaminationInterval:
-		if isStillOnDangerArea: 
-			GameManager.addContaminationProgress()
 		
 func _change_direction() -> void:
 	platformAreaX = platform_area.shape.get_rect().size.x
 	platformAreaY = platform_area.shape.get_rect().size.y
 	direction = Vector2(randi_range(-platformAreaX, platformAreaX), randi_range(-platformAreaY, platformAreaY)).normalized()
 	direction_cooldown_timer.start()
+	
+func _animate_sprite() -> void:
+	if direction:
+		animation_player.play("run")
+		
+	if direction.x < 0:
+		sprite_2d.flip_h = false
+	elif direction.x > 0:
+		sprite_2d.flip_h = true
 
 func _on_warning_area_area_entered(area: Area2D) -> void:
 	print('warning')
@@ -65,14 +85,6 @@ func _on_danger_area_area_exited(area: Area2D) -> void:
 
 func _on_direction_cooldown_timer_timeout() -> void:
 	_change_direction()
-
-
-func _on_mouse_entered() -> void:
-	isHovered = true
-
-
-func _on_mouse_exited() -> void:
-	isHovered = false
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
